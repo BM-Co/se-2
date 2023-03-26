4218e19f7226fec2bff299b6dd5d63f406929f9c
Abinash Panda <abinash@prodios.com>
2023-03-26 20:08:18 +0530

    ignore typescript build errors

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
